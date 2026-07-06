570925ccee8ffc638b56b9a82f3c78fb7adb2d55
pagarwal884 <pagarwal1145@gmail.com>
2026-07-06 19:28:35 +0530

    Completed the Navbar of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
-    <div className='text-6xl text-center text-blue-700'>
-      Hello World
-    </div>
-  )
-}
+  const [theme, setTheme] = useState('light');
 
-export default App
+  return (
+    <div className={theme === 'dark' ? 'dark' : ''}>
+      <div className="min-h-screen bg-white dark:bg-black relative">
+        <Navbar theme={theme} setTheme={setTheme} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import assets from '../assets/assets.js';
+
+const Navbar = ({ theme, setTheme }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <nav className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
+      <img
+        src={theme === 'dark' ? assets.logo_dark : assets.logo}
+        alt="Logo"
+        className="w-32 sm:w-40"
+      />
+
+      <div
+        className={`flex text-gray-700 dark:text-white sm:text-sm gap-5
+        max-sm:fixed max-sm:top-0 max-sm:bottom-0 max-sm:right-0
+        ${
+          sidebarOpen
+            ? 'max-sm:w-60 max-sm:pl-10'
+            : 'max-sm:w-0 max-sm:overflow-hidden'
+        }
+        max-sm:min-h-screen max-sm:h-full
+        max-sm:flex-col max-sm:bg-primary
+        max-sm:text-white max-sm:pt-20
+        sm:items-center transition-all duration-300`}
+      >
+        <img
+          src={assets.close_icon}
+          alt=""
+          className="w-5 absolute right-4 top-4 sm:hidden cursor-pointer"
+          onClick={() => setSidebarOpen(false)}
+        />
+
+        <a href="#" className="sm:hover:border-b">
+          Home
+        </a>
+
+        <a href="#services" className="sm:hover:border-b">
+          Services
+        </a>
+
+        <a href="#our-work" className="sm:hover:border-b">
+          Our Work
+        </a>
+
+        <a href="#contact-us" className="sm:hover:border-b">
+          Contact Us
+        </a>
+      </div>
+
+      <div className="flex items-center gap-2 sm:gap-4">
+        <img
+          src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon}
+          alt=""
+          className="w-6 sm:hidden cursor-pointer"
+          onClick={() => setSidebarOpen(true)}
+        />
+
+        <a
+          href="#contact-us"
+          className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-105 transition-all"
+        >
+          Connect
+          <img src={assets.arrow_icon} alt="" width={14} />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
